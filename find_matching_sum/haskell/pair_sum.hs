62c315df61aed033a932a3e15ced6714f9824e83
gideonflash <gideonkadzura@gmail.com>
2020-12-22 23:15:47 +0000

    "take some from a list" haskell v2

--- find_matching_sum/haskell/pair_sum.hs
+++ find_matching_sum/haskell/pair_sum.hs
@@ -1,13 +1,29 @@
-pairSum :: Integer -> [Integer] -> Maybe [Integer]
-pairSum _ [] = Nothing
-pairSum num (x : xs) =
-  case pairTest x num xs of
-    Nothing -> pairSum num xs
-    Just (n1, n2) -> Just [n1, n2]
+-- Create a fold, over the list of numbers
+-- When thinking of this solution, i had framed it asin
+-- looking for a construct in the language that gives me the ability
+-- to kick out of the loop when i found my match
+-- But being able to kick out the loop is a best case senario,
+-- what counts is the change of number of operations as the input of the
+-- functions grows.
 
-pairTest :: Integer -> Integer -> [Integer] -> Maybe (Integer, Integer)
-pairTest _ _ [] = Nothing
-pairTest curr target (x : xs) =
-  case x + curr == target of
-    False -> pairTest curr target xs
-    True -> Just (curr, x)
+-- Choosing a record as a way to keep track of things, i have often not used
+-- records and my thinking was that the function you create is the one that is
+-- exposed. So what i have called pair some can be part of some other function
+-- that then extracts what it needs. In terms of run time cost the extraction
+-- is not an operation that counts.
+data Resolver = Store
+  { store :: [Integer],
+    target :: Integer,
+    result :: Maybe (Integer, Integer)
+  }
+  deriving (Show)
+
+initial :: Resolver
+initial = Store [] 10 Nothing
+
+-- Find target pair total
+pairSum :: Integer -> Resolver -> Resolver
+pairSum curr (Store xs trgt Nothing)
+  | trgt - curr `elem` xs = Store xs trgt (Just (curr, trgt - curr))
+  | otherwise = Store (curr : xs) trgt Nothing
+pairSum _ (Store xs trgt (Just res)) = Store xs trgt (Just res)
